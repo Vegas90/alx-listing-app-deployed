625aa50ea235b4860eba4c9fc11b40f9dd70863b
Vegas90 <amosmwoni@gmail.com>
2025-05-29 17:40:36 +0300

    m

---
+++ components/layout/Header.tsx
@@ -0,0 +1,5 @@
+//create a functional component that renders the header content, including search bar, sign and sign up , Types of accommodation (e.g. Rooms, Mansion, Countryside, etc.) and a logo.
+
+import { PropertyProps } from "@/interfaces";
+const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
+  { 
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,21 @@
 // This is a single-line comment
+//define PropertyProps interface from PROPERTYLISTINGSAMPLE IN constants/index.ts
+export interface PropertyProps {
+  name: string; 
+    address: {
+        state: string;
+        city: string;
+        country: string;
+    };
+    rating: number;
+    category: string[];
+    price: number;
+    offers: {
+        bed: string;
+        shower: string;
+        occupants: string;
+    };
+    image: string;      
+    discount: string;
+}
+   
